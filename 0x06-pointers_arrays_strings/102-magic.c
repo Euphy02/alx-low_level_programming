e9a90d6956f9814e9c11bb17d67c62b4766fde99
Euphy02 <euphemiaonyinn08@gmail.com>
2023-03-01 21:10:56 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 int main(void)
 {
@@ -18,9 +17,9 @@ int main(void)
  * - you are not allowed to code anything else than this line of code
  */
 
-	*(p + 5) = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
-        printf("a[2] = %d\n", a[2]);
-        return (0);
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
 
